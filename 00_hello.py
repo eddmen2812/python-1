0cd5dfe07784f67147e8f3388b5608e0beffc346
Edison Mendoza <eypclark@hotmail.com>
2024-02-08 14:41:43 -0500

    hola mundo

---
+++ 00_hello.py
@@ -0,0 +1,21 @@
+# hola mundo
+print ("Hola mundo")
+print ('Hola mundo')
+
+
+"""comentoario
+ comentario
+comentario
+"""
+'''
+comentarios 2
+cometarios 2
+'''
+
+# consulta tipo de dato
+print(type("hola puthon type")) # tipo 'str'
+print(type(5)) # tipo 'int'
+print(type(1.5)) # tipo 'double'
+print(type(1 + 1j)) # tipo 'complex'
+print(type(True)) # tipo 'boolean'
+
