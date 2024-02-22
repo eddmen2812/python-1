a3d394b1cc04a85c90684bead7aa142c40f4a56e
Edison Mendoza <eypclark@hotmail.com>
2024-02-21 23:06:55 -0500

    Tipoes de Hists

---
+++ backend/type_hits.py
@@ -0,0 +1,16 @@
+# Types hits
+my_string = "hola python"
+print(f"{type(my_string)}")
+
+my_other_s = 5
+print(my_other_s)
+print(type(my_string))
+
+my_type_v: str = "My tipo de variable"
+
+print(my_type_v)
+print(type(my_string))
+
+my_type_v = 5
+print(my_type_v)
+print(type(my_type_v))
